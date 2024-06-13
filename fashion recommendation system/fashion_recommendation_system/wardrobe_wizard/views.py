cf6091372ef139d1edcc9a22fe5d8ea2e2ab1bc2
avick saha <avicksaha17@gmail.com>
2024-06-13 13:49:43 +0530

    add signup feature

--- fashion recommendation system/fashion_recommendation_system/wardrobe_wizard/views.py
+++ fashion recommendation system/fashion_recommendation_system/wardrobe_wizard/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+def signupUser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password
+            =password)
+            user.save()
+    return render(request, 'signup.html')
