4e010c0b1b260b877cd05c9ea0912c63d7f14cbb
avick saha <avicksaha17@gmail.com>
2024-06-14 14:20:32 +0530

    add upload photo feature

--- fashion recommendation system/fashion_recommendation_system/wardrobe_wizard/views.py
+++ fashion recommendation system/fashion_recommendation_system/wardrobe_wizard/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from .models import Photo
 
 # Create your views here.
-from django.http import HttpResponse
+def home(request):
+    if request.user.is_authenticated:
+        photos = Photo.objects.filter(user=request.user)
+        return render(request, 'home.html', {'photos': photos})
+    else:
+        message = "You have to be logged in to see your uploaded photos."
+        return render(request, 'home.html', {'message': message})
 
-
-def index(request):
-    return render(request, 'index.html')
-
-def signupUser(request):
+def user_signup(request):
     if request.method == 'POST':
         username = request.POST['username']
         first_name = request.POST['first_name']
@@ -21,4 +28,34 @@ def signupUser(request):
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password
             =password)
             user.save()
+            return redirect('user_login')
     return render(request, 'signup.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
+
+def upload_photo(request):
+    if not request.user.is_authenticated:
+        message = "You have to be logged in to upload photos."
+        return render(request, 'upload_photo.html', {'message': message})
+    
+    if request.method == 'POST' and request.FILES.get('photo'):
+        photo = request.FILES['photo']
+        fs = FileSystemStorage()
+        filename = fs.save(photo.name, photo)
+        uploaded_file_url = fs.url(filename)
+        Photo.objects.create(user=request.user, photo=filename)
+        return redirect('home')
+    
+    return render(request, 'upload_photo.html')
